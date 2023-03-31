0052c10ae32b328d0ec4e523a048c5fa98cb5090
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-03-31 11:51:46 -0600

    Navigation Controller y animación de letras con ciclo for.
    Se crearon las conexiones entre los View Controllers, utilizando el modo "show" y envolviendo a la vista origen en un "Navigation Controller" para que se agregue la barra de navegación entre vistas automáticamente ya con los Segues creados.
    También se creó una pequeña animación de letras en la pantalla de inicio, usando el ciclo for y un Timer que tiene un pequeño truco en cómo cuenta el tiempo para cada ciclo.

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -3,14 +3,25 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        titleLabel.text = "" // Dejamos en blanco la etiqueta del título de la app
+        var count = 0.0 // Agregamos el punto decimal para que sea de tipo Double
+        let titleText = "⚡️FlashChat" // Usamos Control+Command+Espacio para el teclado de emojis
+        for letter in titleText { // Para todos los elementos (letras) que se encuentren en la variable titleText (cadena)...
+            /*  Abrimos el timer correcto con la opción que tiene el "Timer" solito, después escribimos un punto y buscamos "scheduledTimer" y elegimos el que tiene "withTimeInterval"
+                Necesitamos multiplicar el intervalo por la variable incrementable "count", porque aunque esté dentro de un lop, el tiempo se calcula para todos los ciclos, desde el
+                instante en que se inicia la estructura FOR, entonces si no los diferenciamos con esta multiplicación, todas las letras se imprimirían 0.1 segundos después de iniciar
+                el ciclo for */
+            Timer.scheduledTimer(withTimeInterval: 0.1 * count, repeats: false) { timer in
+                self.titleLabel.text?.append(letter) // Usamos el método .append() para ir agregando las letras al atributo "text" de nuestra variable titleLabel.
+            }
+            count += 1 // Modificamos el contador para que cada letra se imprima en un tiempo distinto e incrementable, desde el inicio del ciclo for.
+        }
     }
     
-
 }
+ 
